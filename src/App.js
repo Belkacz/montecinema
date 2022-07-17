55798c40b1711cfcabc5771fc8a52639faec9348
Belkacz <belka.lukasz.job@gmail.com>
2022-07-17 16:24:02 +0200

    123

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logo.svg";
+import eye from "./assets/eye.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+
+      <div className="body-frame">
+        <div className="header-frame">
+          <p className="font--header">
+            Ahoy you! Care to register?
+            {/* <p className="font--header" style={{ color: "grey" }}>
+              Care to register?
+            </p> */}
+          </p>
+        </div>
+        <div className="form-frame">
+          <form className="form">
+            <div className="form">
+              <label className="label">
+                email
+                <div>
+                  <input
+                    className="input"
+                    placeholder="Enter your password"
+                  ></input>
+                </div>
+              </label>
+              <br></br>
+              <label className="label">
+                password
+                <div>
+                  <img src={eye} className="eye-logo" alt="eye" />
+                  <input
+                    className="input"
+                    placeholder="Enter your password"
+                  ></input>
+                </div>
+              </label>
+              <div className="validation-text"></div>
+            </div>
+          </form>
+        </div>
+        <div className="buttons-frame">
+        <div className="next-frame">
+          <p className="next-text">Next Step</p>
+        </div>
+        <div className="login-frame">
+          <p className="login-text">Login instead</p>
+        </div>
+        </div>
+
+      </div>
     </div>
   );
 }
