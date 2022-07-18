1ec482cfee9c309a88a25e62efa0285986283276
Belkacz <belka.lukasz.job@gmail.com>
2022-07-18 08:07:31 +0200

    checkbox fix

--- src/Page2.js
+++ src/Page2.js
@@ -8,9 +8,9 @@ function Page2({ reciveName, pageswap, name }) {
   let minAge = 18;
   const [firstname, setFName] = useState("");
   const [lastname, setLName] = useState("");
-  const [ageold, connectAge] = useAgeValidtor("", minAge);
   const [age, setAge] = useState();
   const [msg, setMsg] = useState(null);
+  const [checkbox, setCheckbox] = useState(false);
 
   const checkfunction = (data) => {
     if (typeof reciveName === "function") {
@@ -41,10 +41,15 @@ function Page2({ reciveName, pageswap, name }) {
       setMsg(null);
     }
   };
+  const toggleCheckbox = (e) => {
+    e.preventDefault();
+    setCheckbox(!checkbox);
+  };
+
   useEffect(() => {
     calculateAge();
   }, [age]);
-  console.log(age);
+
   return (
     <div>
       <div className="header-frame">
@@ -53,70 +58,75 @@ function Page2({ reciveName, pageswap, name }) {
       <div className="form-frame">
         <form className="form">
           <div className="form">
-          <div className="input-frame">
-            <label className="label">
-              First Name
-              <div>
-                <input
-                  onChange={(e) => checkfunction(e.target.value)}
-                  className="input"
-                  placeholder="e.g Jessica"
-                ></input>
-              </div>
-            </label>
+            <div className="input-frame">
+              <label className="label">
+                First Name
+                <div>
+                  <input
+                    onChange={(e) => checkfunction(e.target.value)}
+                    className="input"
+                    placeholder="e.g Jessica"
+                  ></input>
+                </div>
+              </label>
             </div>
             <br></br>
             <div className="input-frame">
-            <label className="label">
-              Last Name
-              <div>
-                <input
-                  onChange={(e) => setLName(e.target.value)}
-                  className="input"
-                  placeholder="e.g Walton"
-                ></input>
-              </div>
-            </label>
+              <label className="label">
+                Last Name
+                <div>
+                  <input
+                    onChange={(e) => setLName(e.target.value)}
+                    className="input"
+                    placeholder="e.g Walton"
+                  ></input>
+                </div>
+              </label>
             </div>
             <br></br>
             <div className="input-frame">
-            <label className="label">
-              Date of Birth
-              <div>
-                <input
-                  value={age}
-                  type="date"
-                  onChange={(e) => setAge(e.target.value)}
-                  className="input"
-                  placeholder="DD/MM/YYYY"
-                ></input>
-              </div>
-            </label>
+              <label className="label">
+                Date of Birth
+                <div>
+                  <input
+                    value={age}
+                    type="date"
+                    onChange={(e) => setAge(e.target.value)}
+                    className="input"
+                    placeholder="DD/MM/YYYY"
+                  ></input>
+                </div>
+              </label>
             </div>
             <div className="validation-frame">
-            <div className="validation-text">
-              <p>{msg}</p>
-            </div>
+              <div className="validation-text">
+                <p>{msg}</p>
+              </div>
             </div>
             <div className="input-frame">
-            <div className="chackboxframe">
-              <input
-                value={age}
-                type="checkbox"
-                onChange=""
-                className="checkbox"
-              ></input>
-              <label className="label">I accept Privacy Policy</label>
+              <div className="chackboxframe">
+                <input
+                  value={age}
+                  type="checkbox"
+                  onChange={(e) => {
+                    toggleCheckbox(e);
+                  }}
+                  className="checkbox"
+                ></input>
+                <label className="label">I accept Privacy Policy</label>
+              </div>
             </div>
-            </div>
-            
           </div>
         </form>
       </div>
       <div className="buttons-frame">
         <button
           disabled={
-            age < 18 || msg != null || name == "" || lastname == ""
+            age < 18 ||
+            msg != null ||
+            name == "" ||
+            lastname == "" ||
+            checkbox === false
               ? true
               : false
           }
